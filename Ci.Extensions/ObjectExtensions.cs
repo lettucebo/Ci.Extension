376fb0eebb19f095ef645d896e6f42a70448b9e3
紙鈔魚 <lettucebo@hotmail.com>
2016-07-13 17:05:37 +0800

    feat: add some method

---
+++ Ci.Extensions/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ci.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// null轉為空字串
+        /// </summary>
+        /// <param name="field">The field.</param>
+        /// <returns>string</returns>
+        public static string FieldToString(this object field)
+        {
+            return field == null ? string.Empty : field.ToString().Trim();
+        }
+    }
+}
---
+++ Ci.Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ci.Extensions
+{
+    /// <summary>
+    /// StringExtensions
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 字串null也去空白
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string ToTrim(this string value)
+        {
+            return value == null ? string.Empty : value.Trim();
+        }
+    }
+}
