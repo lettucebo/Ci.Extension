03c132b3ec499eb104927545e81ddbef89367a39
Money <abc12207@gmail.com>
2017-01-10 18:29:52 +0800

    add HttpPostedFileBase HasFile function

---
+++ Ci.Extension.Web/HttpPostedFileBaseExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Ci.Extensions.Web
+{
+    public static class HttpPostedFileBaseExtensions
+    {
+        public static bool HasFile(this HttpPostedFileBase file)
+        {
+            return (file != null && file.ContentLength > 0);
+        }
+    }
+}
