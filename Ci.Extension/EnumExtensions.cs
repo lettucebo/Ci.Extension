095e582a6fa1b08629b528e3674b752723fd38a1
Money <abc12207@gmail.com>
2017-06-13 10:44:45 +0800

    如上

--- Ci.Extension/EnumExtensions.cs
+++ Ci.Extension/EnumExtensions.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ci.Extensions
 {
@@ -23,10 +25,26 @@ namespace Ci.Extensions
             DescriptionAttribute[] attributes =
                 (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
 
-            if (attributes != null && attributes.Length > 0)
+            if (attributes.Any())
                 return attributes[0].Description;
             else
                 return value.ToString();
+        }
+
+        /// <summary>
+        /// Retrieves the <see cref="DisplayAttribute.Name" /> property on the <see cref="DisplayAttribute" />
+        /// of the current enum value, or the enum's member name if the <see cref="DisplayAttribute" /> is not present.
+        /// </summary>
+        /// <param name="value">This enum member to get the name for.</param>
+        /// <returns>The <see cref="DisplayAttribute.Name" /> property on the <see cref="DisplayAttribute" /> attribute, if present.</returns>
+        public static string GetDisplayName(this Enum value)
+        {
+            return value.GetType()
+                       .GetMember(value.ToString())
+                       .FirstOrDefault()
+                       ?.GetCustomAttribute<DisplayAttribute>(false)
+                       ?.Name
+                   ?? value.ToString();
         }
     }
 }
