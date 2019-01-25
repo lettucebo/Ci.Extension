0b3ab17fd2670cc325a9b0ad7cde06b3cd2653a4
Money Yu <abc12207@gmail.com>
2019-01-25 20:23:42 +0800

    add IListExtension

---
+++ Ci.Extension/IListExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+
+namespace Ci.Extension
+{
+    public static class IListExtensions
+    {
+        public static DataTable ToDataTable<T>(this IList<T> datas) where T : class
+        {
+            var properties = typeof(T).GetProperties();
+            var table = new DataTable();
+            var instance = datas.FirstOrDefault();
+            if (instance != null)
+            {
+                foreach (var property in properties)
+                {
+                    var type = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                    var name = property.Name;
+                    var annotation = instance.GetAttributeFrom<DisplayNameAttribute>(property.Name);
+                    var displayName = annotation?.DisplayName;
+                    table.Columns.Add(displayName ?? name, type);
+                }
+            }
+            foreach (var entity in datas)
+            {
+                table.Rows.Add(properties.Select(p => p.GetValue(entity, null)).ToArray());
+            }
+            return table;
+        }
+    }
+}
