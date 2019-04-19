eab15f20a264c54cd760235bc2f96e807b2978a4
Money Yu <abc12207@gmail.com>
2019-04-19 18:06:34 +0800

    add string ReplaceSpace

--- Ci.Extension.ShareCode/StringExtensions.cs
+++ Ci.Extension.ShareCode/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Ci.Extension
@@ -43,6 +44,38 @@ namespace Ci.Extension
             };
 
             return value.Trim(allWhitespaces);
+        }
+
+        /// <summary>
+        /// Replcae all possible space from input string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ReplaceSpace(this string value)
+        {
+            string[] allWhitespaces = new string[] {
+                // SpaceSeparator category
+                "\u0020", "\u1680", "\u180E", "\u2000", "\u2001", "\u2002", "\u2003",
+                "\u2004", "\u2005", "\u2006", "\u2007", "\u2008", "\u2009", "\u200A",
+                "\u202F", "\u205F", "\u3000",
+                // LineSeparator category
+                "\u2028",
+                // ParagraphSeparator category
+                "\u2029",
+                // Latin1 characters
+                "\u0009", "\u000A", "\u000B", "\u000C", "\u000D", "\u0085", "\u00A0",
+                // ZERO WIDTH SPACE (U+200B) & ZERO WIDTH NO-BREAK SPACE (U+FEFF)
+                "\u200B", "\uFEFF"
+            };
+
+            StringBuilder sb = new StringBuilder(value);
+
+            foreach (string whitespace in allWhitespaces)
+            {
+                sb.Replace(whitespace, string.Empty);
+            }
+
+            return sb.ToString();
         }
 
         /// <summary>
