7802db5ae6309136df9666ce1a65ad27991fe0b1
Money <abc12207@gmail.com>
2019-10-03 16:48:32 +0800

    add Ci.Extension.AspNetCoreApp

---
+++ Ci.Extension.AspNetCoreApp/UrlHelperExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace Ci.Extension.AspNetCoreApp
+{
+    public static class UrlHelperExtension
+    {
+        public static string AdaptContent(this UrlHelper url, string contentPath)
+        {
+            if (contentPath.StartsWith("http"))
+                return contentPath;
+
+            if (contentPath.StartsWith("~"))
+                return url.Content(contentPath);
+
+            throw new NotSupportedException($"{contentPath} string is not support.");
+        }
+    }
+}
