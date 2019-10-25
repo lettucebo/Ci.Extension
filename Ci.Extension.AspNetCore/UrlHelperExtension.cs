c76629814be3026ef9d9bbeb4fa498d442f4fd1a
Money <abc12207@gmail.com>
2019-10-25 18:17:47 +0800

    add AbsoluteContent and AbsoluteAction

--- Ci.Extension.AspNetCore/UrlHelperExtension.cs
+++ Ci.Extension.AspNetCore/UrlHelperExtension.cs
@@ -15,5 +15,23 @@ namespace Ci.Extension.AspNetCore
 
             throw new NotSupportedException($"{contentPath} string is not support.");
         }
+
+        /// <summary>
+        /// Generates a fully qualified URL to the specified content by using the specified content path.
+        /// </summary>
+        /// <param name="url">The URL helper.</param>
+        /// <param name="contentPath">The content path.</param>
+        /// <returns>The absolute URL.</returns>
+        public static string AbsoluteContent(this IUrlHelper url, string contentPath)
+        {
+            var request = url.ActionContext.HttpContext.Request;
+            return new Uri(new Uri(request.Scheme + "://" + request.Host.Value), url.Content(contentPath)).ToString();
+        }
+
+        public static string AbsoluteAction(this IUrlHelper url, string action, string controller, object routeValues = null)
+        {
+            string scheme = url.ActionContext.HttpContext.Request.Scheme;
+            return url.Action(action, controller, routeValues, scheme);
+        }
     }
 }
