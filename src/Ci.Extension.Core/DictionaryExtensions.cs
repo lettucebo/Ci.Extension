d05d5ec2d7a949a9e609c2c1dba7fca30483554a
Money Yu <abc12207@gmail.com>
2022-07-26 12:17:45 +0800

    add DictionaryExtensions

---
+++ src/Ci.Extension.Core/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Ci.Extension.Core
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> dictToAdd)
+        {
+            foreach (var dict in dictToAdd)
+            {
+                source.Add(dict);
+            }
+        }
+    }
+}
